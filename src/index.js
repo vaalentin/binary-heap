ea8905c347a9859064071c50f159cf5c5c42f688
Vaalentin <vaalentin@protonmail.com>
2016-01-27 18:14:43 +0100

    add basic push/pop

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 export default class BinaryHeap {
-  isEmpty() {
+  constructor() {
+    this._nodes = [];
+    this._heapSize = 0;
+  }
 
+  isEmpty() {
+    return this._heapSize <= 0;
   }
 
   push(node) {
-
+    this._heapSize++;
+    this._nodes.push(node);
   }
 
   pop() {
+    const node = this.peek();
+    this._nodes[0] = this._nodes[this._heapSize - 1];
+    this._heapSize--;
+    return node;
+  }
 
+  peek() {
+    if(this.isEmpty()) {
+      throw('Heap is empty');
+    } else {
+      return this._nodes[0];
+    }
   }
 
   update(node) {
